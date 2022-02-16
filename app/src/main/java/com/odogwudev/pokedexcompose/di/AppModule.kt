9c36396d22c2f139551718e5bc4d0d47061915ed
Michael Enoma <odogwudev@e.email>
2022-02-16 15:02:51 +0100

    Setting up Network Call

--- app/src/main/java/com/odogwudev/pokedexcompose/di/AppModule.kt
+++ app/src/main/java/com/odogwudev/pokedexcompose/di/AppModule.kt
@@ -1,11 +1,34 @@
 package com.odogwudev.pokedexcompose.di
 
+import com.google.gson.Gson
+import com.odogwudev.pokedexcompose.data.remote.PokeApi
+import com.odogwudev.pokedexcompose.repository.PokemonRepository
+import com.odogwudev.pokedexcompose.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule{
+object AppModule {
 
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
 }
---
+++ app/src/main/java/com/odogwudev/pokedexcompose/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.odogwudev.pokedexcompose.util
+
+object Constants {
+
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+}
