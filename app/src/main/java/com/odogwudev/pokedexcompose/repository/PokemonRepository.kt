0f4f61b9c9f0bd411726fc1917ee30278e020cb4
Michael Enoma <odogwudev@e.email>
2022-02-16 14:53:47 +0100

    Repository Setup

--- app/src/main/java/com/odogwudev/pokedexcompose/repository/PokemonRepository.kt
+++ app/src/main/java/com/odogwudev/pokedexcompose/repository/PokemonRepository.kt
@@ -1,4 +1,35 @@
 package com.odogwudev.pokedexcompose.repository
 
-class PokemonRepository {
+import com.odogwudev.pokedexcompose.data.remote.PokeApi
+import com.odogwudev.pokedexcompose.data.remote.responses.Pokemon
+import com.odogwudev.pokedexcompose.data.remote.responses.PokemonList
+import com.odogwudev.pokedexcompose.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import java.lang.Exception
+import javax.inject.Inject
+
+//last lifetime of project
+@ActivityScoped
+class PokemonRepository @Inject constructor(
+    private val api: PokeApi
+) {
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occurred")
+        }
+
+        return Resource.Success(response)
+    }
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(pokemonName)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occurred")
+        }
+
+        return Resource.Success(response)
+    }
 }
